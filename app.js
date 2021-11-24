96dc80f67759e5c487b327a147ad700dea1e47ab
Rox <you@example.com>
2021-11-24 08:57:15 +0100

    session

--- app.js
+++ app.js
@@ -1,35 +1,94 @@
 //Middleware for .env
 require("dotenv/config");
-require("./db");
+
+//Import Mongo connection
+require("./db/index.js");
+
+//Variables
 const express = require("express");
-const axios = require('axios')
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
-
 const app = express();
+const chalk = require('chalk');
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 5000
+const MONGO_URI = process.env.MONGODB_URI;
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
 
-const projectName = "test";
+//Middleware de hbs
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+
+//Middleware de body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//Middleware para archivos estaticos
+app.use(express.static(__dirname + "/public"));
+
+// //Middleware de sessions
+require("./config/session.config")(app);
+
+
+
+
+// //Mongoose connect
+// const connectToMongo = async () => {
+//     try {
+//       await mongoose.connect(MONGO_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//       });
+//       console.log(chalk.bgBlue("Connected to Mongo"));
+//     } catch (err) {
+//       console.log(chalk.bgRed("Error:", err));
+//     }
+//   };
+  
+// connectToMongo();
+
+
+
+
+
+
+
+
+const axios = require('axios')
+
+
+
+
+// // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+// require("./config")(app);
+
+// const projectName = "test";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 const getDisney = async () => {
-app.locals.response =  await axios.get('http://api.disneyapi.dev/characters')
+    app.locals.response =  await axios.get('http://api.disneyapi.dev/characters')
 app.locals.disney = app.locals.response.data.data;
-console.log(app.locals.response.data.data)
+// console.log(app.locals.response.data.data)
 }
+
 getDisney();
-// 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
 
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
+
+
+
+
+
+//App listener
+app.listen(PORT, () => {
+    console.log(chalk.bgGreen(`Server running in port ${PORT}`));
+  });
---
+++ models/Character.model.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const characterSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    films: { type: String },
+    tvShows: { type: String },
+    imageUrl: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Character = model("Character", characterSchema);
+
+module.exports = Character;
